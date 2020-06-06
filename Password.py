e10501e9ba88ba9cd58792bdda4c866a21c6b6f3
Turkish234 <66484710+Turkish234@users.noreply.github.com>
2020-06-06 22:37:00 +0530

    Update Password.py

--- Password.py
+++ Password.py
@@ -1,4 +1,4 @@
-password = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+password = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f", "g", "h", "i"] #you can also add alphabets to Z and symbols like @,#.
 
 for i in password :
 
@@ -7,7 +7,12 @@ for i in password :
 		for k in password:
 
 			for l in password:
-				q = i + j + k + l
-				f =open("password-0.txt","a")
-				f.write(q+'\n')
-				f.close()								
+				
+				for m in password:
+					
+					for n in password:
+						
+						q = i + j + k + l +m + n
+						f =open("password-0.txt","a")
+						f.write(q+'\n')
+						f.close()											
